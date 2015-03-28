3167d13e3d89b487ac5d5c9ea6e542e818587c01
linoor <michal.pomaranski@gmail.com>
2015-03-28 21:38:02 +0100

    added boid.h

---
+++ boid.h
@@ -0,0 +1,23 @@
+class Boid {
+
+public:
+    void set_x(double x) {
+        this->pos_x = x;
+    }
+    void set_y(double y) {
+        this->pos_y = y;
+    }
+    
+    Boid(double pos_x, double pos_y, double pos_z) {
+        this->pos_x = pos_x;
+        this->pos_y = pos_y;
+        this->pos_z = pos_z;
+    }
+private:
+    
+    double pos_x;
+    double pos_y;
+    double pos_z;
+
+
+};
