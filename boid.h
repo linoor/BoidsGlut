8debf63e47c3d1432ede18559675cb7e833aeb7a
linoor <michal.pomaranski@gmail.com>
2015-04-10 21:28:51 +0200

    changed whitespace in boid.h

--- boid.h
+++ boid.h
@@ -18,19 +18,24 @@ public:
     void set_x(double x);
     void set_y(double y);
     void set_z(double z);
+
     void set_vel_x(double vel_x);
     void set_vel_y(double vel_y);
     void set_vel_z(double vel_z);
+
     double get_vel_x() const;
     double get_vel_y() const;
     double get_vel_z() const;
+
     double get_x() const;
     double get_y() const;
     double get_z() const;
 
     std::vector<double> get_vector();
-    bool operator==(const Boid& other);
     double get_distance(Boid boid);
+
+    bool operator==(const Boid& other);
+    
     Boid(double pos_x, double pos_y, double pos_z);
     Boid(double pos_x, double pos_y, double pos_z, double vel_x, double vel_y, double vel_z);
 };
